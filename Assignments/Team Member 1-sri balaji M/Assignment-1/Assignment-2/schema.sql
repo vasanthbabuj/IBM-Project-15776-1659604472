c46f02f895d6e6fe01e5d8504ffa09ed22fab45f
sribalajimuruganandham <91721102+sribalajimuruganandham@users.noreply.github.com>
2022-10-15 13:55:55 +0530

    Delete Assignments/Team Member 1-sri balaji M/Assignment-1/Assignment-2 directory

--- Assignments/Team Member 1-sri balaji M/Assignment-1/Assignment-2/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL
-);
